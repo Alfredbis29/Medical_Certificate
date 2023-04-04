cc12ae7de4570d6898ff076f45f67acf6d4360ec
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-04 15:01:36 +0200

    Set up the associations between models

--- app/models/medical_certificate.rb
+++ app/models/medical_certificate.rb
@@ -1,3 +1,5 @@
 class MedicalCertificate < ApplicationRecord
   belongs_to :patient
+  belongs_to :user
+  accepts_nested_attributes_for :patient
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :medical_certificates
+  enum role: [:admin, :doctor, :nurse]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :nurse
+  end
 end
