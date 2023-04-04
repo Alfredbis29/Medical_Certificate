0e2d5da9e5f2fae0809317555284d1706c17d1d5
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-04 16:03:34 +0200

    fix the path and routing error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  has_many :medical_certificates
-  enum role: [:admin, :doctor, :nurse]
-  after_initialize :set_default_role, :if => :new_record?
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  def set_default_role
-    self.role ||= :nurse
+  def self.authentication_keys
+    [:email]
   end
 end
