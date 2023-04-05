37b45ca1ea06d0588eb1db12b575ff6a1308c000
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-05 23:03:03 +0200

    add pundit for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
-  include Pundit
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  include Pundit::Authorization
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+    protected
 
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+        # redirect_to root_path
+    end
 end
